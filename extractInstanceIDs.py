77d9c1713c3145766dd447eec54bd3b4bd3fe5c4
mshoaibshafi@yahoo.com <mshoaibshafi@yahoo.com>
2019-04-07 17:37:38 -0700

    fixed extractInstanceIDs for single ID as well as strip newline character at the end of IDs

--- extractInstanceIDs.py
+++ extractInstanceIDs.py
@@ -9,7 +9,14 @@ def extractInstanceIDs(file):
 		for line in f.readlines(): 
 			# split by commas
 			# Only works if all instances IDs are on a single line
-			inst_ids_list = line.split(',')
+			# If there are multiple instance IDs and separated by ','
+			# The last entry in the list coming up with '\n', so do rstrip to erase '\n' before splitting
+			print (line)
+			if ',' in line:
+				inst_ids_list = line.rstrip('\n').split(',')
+			else:
+				# If there is only one instance ID in the file then split by '\n'
+				inst_ids_list = line.rstrip('\n').split('\n')
 		return inst_ids_list
 	
 
